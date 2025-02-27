5897695f3d4310ba297618f3f927fa0574c1b0ba
bdoman448 <bruno.domancic@gmail.com>
2025-02-27 10:43:23 +0100

    add base typescript validation setup

--- app/ShippingForm.tsx
+++ app/ShippingForm.tsx
@@ -1,28 +1,46 @@
-'use client'
-import { ContentClient } from 'dc-delivery-sdk-js';
-import Image from 'next/image'
+import { ContentClient, ContentItem } from 'dc-delivery-sdk-js';
 
-interface amplienceForm {
-    addressLine1: Object;
-    addressLine2: Object;
-    banner: Object;
-    city: Object,
-    postcode: Object;
+// MISTAKES WERE MADE SHOULD NOT HAVE GONE STRAIGHT FOR LOCALIZATION VALUES
+
+// Move to definitions
+type amplienceLocalizationValue = {
+    values: {values: string}[]
 }
-
-
+type amplienceForm = {
+    addressLine1: amplienceLocalizationValue;
+    addressLine2: amplienceLocalizationValue;
+    city: amplienceLocalizationValue;
+    postcode: amplienceLocalizationValue,
+    // WHEN YOU FIGURE OUT IMAGE SPECIFY BETTER OBJ
+    banner: Object; 
+}
 
 export default async function ShippingForm() {
     const client = new ContentClient({ hubName: 'anorakwaterpolo' });
     const deliveryKey = 'shipping-form-en';
+  
+
     async function fetchForm() {
-        return await client.getContentItemByKey(deliveryKey);
+        const response= await client.getContentItemByKey(deliveryKey);
+        // TS ALWAYS ASSUMES WORST CASE SCENARIO
+        // CHECK IF ACTUALLY RECEIVED CORRECT DATA
+        console.log(response)
+        return response.body
     }
 
+
+
+    function isAmplienceForm(data:any) {
+        if(data && typeof data === "object") {
+
+        }
+    }
+ 
     const form = await fetchForm()
-    const { addressLine1, addressLine2, city, postcode, banner } = form.body
+    const { addressLine1, addressLine2, city, postcode, banner } = form
 
     return (
+        // SEPARATE INTO CHILD COMPONENT WITH USE CLIENT
         <form>
             <label>{addressLine1.values[0].value}</label>
             <input></input>
