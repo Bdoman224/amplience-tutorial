cfa4eb5e88048d082a83fcb52f1d88cfac260c2d
bdoman448 <bruno.domancic@gmail.com>
2025-02-27 16:18:14 +0100

    add base setup for validation

--- app/ShippingFormFields.tsx
+++ app/ShippingFormFields.tsx
@@ -1,32 +1,97 @@
-// 'use client'
-import { Input, Box, Stack } from "@chakra-ui/react"
-import { Field } from '@/components/ui/field';
+"use client";
+import { Input, Box, Stack } from "@chakra-ui/react";
+import { Field } from "@/components/ui/field";
+import { useState } from "react";
+import { z } from "zod"
 
 type amplienceProps = {
-    address1: string;
-    address2: string;
-    city: string;
-    postcode: string,
-    image: string
+  address1: string;
+  address2: string;
+  city: string;
+  postcode: string;
+  image: string;
+};
+
+interface formFields {
+  address1: string;
+  address2: string;
+  city: string;
+  postcode: string;
 }
 
-export default function ShippingFormFields({ address1, address2, city, postcode, image }: amplienceProps) {
-    return (
-        <Box bgAttachment="fixed" className="rounded" bgImage={`url(${image})`} width="1000px" height="1000px">
-            <Stack className='gap-y-4 px-8 py-10'>
-                <Field label={address1} required>
-                    <Input placeholder="George Wash Street" color="black" bg="white" />
-                </Field>
-                <Field label={address2} required>
-                    <Input placeholder="Apt 15" color="black" bg="white" />
-                </Field>
-                <Field label={city} required>
-                    <Input placeholder="San Diego" color="black" bg="white" />
-                </Field>
-                <Field label={postcode} required>
-                    <Input placeholder="12132131" type='number' color="black" bg="white" />
-                </Field>
-            </Stack>
-        </Box>
-    )
+const formSchema = z.object({
+  address1: z.string().min(5, "Address must be at minimum 5 characters long"),
+  address2: z.string().min(2, "Street number must be at minimum 2 characters long"),
+  city: z.string().min(2, "City must be at minimum 2 characters long"),
+  postcode: z.string().min(2, "Postcode must be at minimum 2 characters long"),
+})
+
+export default function ShippingFormFields({
+  address1,
+  address2,
+  city,
+  postcode,
+  image,
+}: amplienceProps) {
+
+  const [formFields, setFormFields] = useState<formFields>({
+    address1: "",
+    address2: "",
+    city: "",
+    postcode: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement> ) => {
+    console.log('AAAA', e)
+    setFormFields({...formFields, [e.target.name]: e.target.value})
+  }
+
+  return (
+    <Box
+      bgAttachment="fixed"
+      className="rounded"
+      bgImage={`url(${image})`}
+      width="1000px"
+      height="1000px"
+    >
+      <Stack className="gap-y-4 px-8 py-10">
+        <Field label={address1} required errorText='This is required'>
+          <Input
+            placeholder="George Wash Street"
+            color="black"
+            bg="white"
+            value={formFields.address1}
+            onChange={handleChange}
+          />
+        </Field>
+        <Field label={address2} required>
+          <Input
+            placeholder="Apt 15"
+            color="black"
+            bg="white"
+            value={formFields.address2}
+            onChange={(e) => handleChange(e)}
+          />
+        </Field>
+        <Field label={city} required>
+          <Input
+            placeholder="San Diego"
+            color="black"
+            bg="white"
+            value={formFields.city}
+            onChange={handleChange}
+          />
+        </Field>
+        <Field label={postcode} required>
+          <Input
+            placeholder="12132131"
+            color="black"
+            bg="white"
+            value={formFields.postcode}
+            onChange={handleChange}
+          />
+        </Field>
+      </Stack>
+    </Box>
+  );
 }
--- app/ShippingFormLocalized.tsx
+++ app/ShippingFormLocalized.tsx
@@ -1,4 +1,5 @@
 import { ContentClient, ContentItem } from 'dc-delivery-sdk-js';
+import ShippingFormFields from '@/app/ShippingFormFields';
 
 // MISTAKES WERE MADE SHOULD NOT HAVE WENT FOR LOCALIZATION INSTANTLY
 
@@ -32,17 +33,6 @@ export default async function ShippingForm() {
     const { addressLine1, addressLine2, city, postcode, banner } = form
 
     return (
-        // SEPARATE INTO CHILD COMPONENT WITH USE CLIENT
-        <form>
-            <label>{addressLine1.values[0].value}</label>
-            <input></input>
-            <label>{addressLine2.values[0].value}</label>
-            <input></input>
-            <label>{city.values[0].value}</label>
-            <input></input>
-            <label>{postcode.values[0].value}</label>
-            <input></input>
-            <img src={banner.values[0].value.url().build()} />
-        </form>
+        <p>Yo</p>
     )
 }
