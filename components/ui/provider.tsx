ccb4dc773ed250f5b0d7e0c55fb20f06e9d51e60
bdoman448 <bruno.domancic@gmail.com>
2025-02-28 12:12:28 +0100

    chakra UI hydration error

--- components/ui/provider.tsx
+++ components/ui/provider.tsx
@@ -1,15 +1,14 @@
-"use client"
+"use client";
 
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
-import {
-  ColorModeProvider,
-  type ColorModeProviderProps,
-} from "./color-mode"
+import { ChakraProvider, defaultSystem, ClientOnly, Skeleton } from "@chakra-ui/react";
+import { ColorModeProvider, type ColorModeProviderProps } from "./color-mode";
 
 export function Provider(props: ColorModeProviderProps) {
   return (
     <ChakraProvider value={defaultSystem}>
-      <ColorModeProvider {...props} />
+      <ClientOnly fallback={<Skeleton boxSize="8" />}>
+        <ColorModeProvider {...props} />
+      </ClientOnly>
     </ChakraProvider>
-  )
+  );
 }
