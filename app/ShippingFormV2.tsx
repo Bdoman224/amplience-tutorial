a50a137d8af993a22769706d8793857d0fc29f38
Bruno Domancic <xg70451@ky2d2rqwdk.dexcom.dexcominc.com>
2025-03-05 10:01:07 +0100

    make home render content dynamically

--- app/ShippingFormV2.tsx
+++ app/ShippingFormV2.tsx
@@ -3,15 +3,10 @@ import { ContentClient } from "dc-delivery-sdk-js";
 import ShippingFormFields from "@/app/ShippingFormFields";
 import { AmplienceProps } from "@/lib/definitions";
 
-export default async function ShippingFormV2() {
+export default async function ShippingFormV2({content}: { content: string }) {
   const client = new ContentClient({ hubName: "anorakwaterpolo" });
-  // HOW TO KNOW WHICH ID TO TARGET?
   async function  fetchForm(): Promise<AmplienceProps> {
-    // const response = await fetch(
-    //   "https://anorakwaterpolo.cdn.content.amplience.net/content/id/d72bed31-0ee9-4468-9838-4695a6ee78f4?depth=all&format=inlined"
-    // );
-    const response = await client.getContentItemById('89b0174c-e82f-436a-8d6e-254deaae0b01')
-    console.log("AAAAAA", response.body);
+    const response = await client.getContentItemById(content);
     const { address1, address2, city, postcode, bannerImage } = response.body;
 
     const localizedFormData = {
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
-import ShippingFormV2 from "@/app/[locale]/v2/ShippingFormV2";
+import ShippingFormV2 from "@/app/ShippingFormV2";
 import { Provider } from "@/components/ui/provider";
 
-export default function Home({params}: { params: { locale: string } }) {
+export default function Home({searchParams}: { searchParams: { locale: string, content: string } }) {
+  console.log("searchParams", searchParams);
+  console.log("content", searchParams.content);
+  console.log("locale", searchParams.locale);
   return (
     <Provider>
-      <p>{params.locale}</p>
       <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <ShippingFormV2 />
+        <ShippingFormV2 content={searchParams.content}/>
       </div>
     </Provider>
   );
